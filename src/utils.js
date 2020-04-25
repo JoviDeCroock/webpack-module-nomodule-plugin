7db7c59b746a942be27727551844fc5f6f23bc0a
Jovi De Croock <decroockjovi@gmail.com>
2020-04-25 12:13:50 +0200

    Tryout domcontentloaded instead of loaded (#12)

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const { loadScript } = require('./constants');
 
 exports.makeLoadScript = (modern, legacy) => `
-addEventListener('load', function() {
+addEventListener('DOMContentLoaded', function() {
   ${(modern.length > legacy.length ? modern : legacy).reduce((acc, _m, i) => `
 ${acc}$l(${modern[i] ? `"${modern[i].attributes.src}"` : undefined}, ${legacy[i] ? `"${legacy[i].attributes.src}"` : undefined})
   `, '').trim()}
