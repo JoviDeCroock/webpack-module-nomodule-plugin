f826e8b5279f188bdcab524e4b6db70915462243
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2020-12-06 14:58:28 +0530

    put the loadScript inside the listener (#23)
    prevents making a global variable

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,9 @@ const { loadScript } = require('./constants');
 
 exports.makeLoadScript = (modern, legacy) => `
 addEventListener('DOMContentLoaded', function() {
+  ${loadScript}
   ${(modern.length > legacy.length ? modern : legacy).reduce((acc, _m, i) => `
 ${acc}$l(${modern[i] ? `"${modern[i].attributes.src}"` : undefined}, ${legacy[i] ? `"${legacy[i].attributes.src}"` : undefined})
   `, '').trim()}
 })
-${loadScript}
 `;
